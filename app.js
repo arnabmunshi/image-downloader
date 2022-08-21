844a09edfac59604ee26d2c9e988ebd84af80005
Arnab Munshi <arnab47stars@gmail.com>
2022-08-21 12:57:48 +0530

    convert download function into aync function

--- app.js
+++ app.js
@@ -23,10 +23,16 @@ const imagePath = "http://65.0.154.115:6500/images/";
 const fs = require("fs");
 const request = require("request");
 
-const download = (url, path, callback) => {
-  request.head(url, (err, res, body) => {
-    request(url).pipe(fs.createWriteStream(path)).on("close", callback);
-  });
+const download = (url, path) => {
+	return new Promise((resolve, reject) => {
+		try {
+			request.head(url, (err, res, body) => {
+				request(url).pipe(fs.createWriteStream(path)).on('close', resolve);
+			});
+		} catch (error) {
+			reject(error);
+		}
+	});
 };
 
 for (let i = 0; i < images.length; i++) {
@@ -34,7 +40,5 @@ for (let i = 0; i < images.length; i++) {
   const url = `${imagePath}${image}`;
   const path = `./images/${image}`;
 
-  download(url, path, () => {
-    console.log("✅ Done!");
-  });
+  await download(url, filePath);
 }
